a52c6319f3bd7038c21b3fb2f911e93856a243ab
墨泠moln <88981453+yourmoln@users.noreply.github.com>
2022-05-02 12:25:01 +0800

    v0.1
    1.添加了获取变量的方法 : $$(dom)
      eg:$$("id")
    
    2.添加了ajax的方法 : sendurl(url,method,boole,sfun,para)
      url(字符串，访问的地址),method(字符串，提交方法，post或get),boole(布尔值，是否异步),sfun(函数，成功后执行的函数),para(字符串，提交的参数):
      eg:
        function ssfun(text){alert(text);}
        sendurl('http://a.com','post',true,ssfun,'a=1&b=2')

---
+++ src/mo.js
@@ -0,0 +1,28 @@
+function $$(dom){//eg:$$("id")
+    return document.getElementById(dom);
+}
+
+function sendurl(url,method,boole,sfun,para) {//url字符串,method字符串,boole布尔值(是否异步),sfun成功函数,para参数字符串
+    //eg:
+    //function ssfun(text){alert(text);}
+    //sendurl('http://a.com','post',true,ssfun,'a=1&b=2')
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttps = new XMLHttpRequest();
+    }else{// code for IE6, IE5
+        xmlhttps = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttps.onreadystatechange = function () {
+        if (xmlhttps.readyState == 4 && xmlhttps.status == 200) {
+            sfun(xmlhttp.responseText);
+        }
+    }
+    if (method = "get"){
+        xmlhttps.open(method, url + "?" + para, boole);
+        xmlhttps.send();
+    }else if(method = "post"){
+        xmlhttps.open(method, url, boole);
+        xmlhttps.send(para);
+    }else{
+        console.log("error:方法名输入错误,应该为'post'或'get'");
+    }
+}
